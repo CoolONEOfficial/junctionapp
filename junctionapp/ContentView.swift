dc370349d27fbf4343ab47e68350f1add1afb75f
Nickolay Trukhin <coolone.official@gmail.com>
2021-11-19 01:49:25 +0300

    Initial Commit

---
+++ junctionapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  junctionapp
+//
+//  Created by Nickolay Truhin on 19.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ junctionapp/junctionappApp.swift
@@ -0,0 +1,17 @@
+//
+//  junctionappApp.swift
+//  junctionapp
+//
+//  Created by Nickolay Truhin on 19.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct junctionappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
